68a36f2c10599c4b3570dee2bb1404ec75eda3f7
Dani <dani.93.perez@gmail.com>
2020-12-29 11:51:07 +0000

    example done

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ var fs = require('fs');
 
 http.createServer(function(req, res) {
     
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    if (req.url === '/') {
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if (req.url === '/api') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        var obj = {
+            firstname: 'Dani',
+            lastname: 'Perez'
+        };
     
-    var obj = {
-        firstname: 'Dani',
-        lastname: 'Perez'
-    };
-
-    res.end(JSON.stringify(obj));
+        res.end(JSON.stringify(obj));
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
 
 }).listen(3000, '192.168.178.227');
