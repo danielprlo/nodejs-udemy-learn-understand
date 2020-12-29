deec448a18836a360efb4d20c215ebeaba57f8ce
Dani <dani.93.perez@gmail.com>
2020-12-29 11:44:12 +0000

    example done

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var fs = require('fs');
 
 http.createServer(function(req, res) {
     
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    var html = fs.readFileSync(__dirname + '/index.html', 'utf-8');
-    var message = 'Hello world...';
-    html = html.replace('{Message}', message);
-    res.end(html);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    
+    var obj = {
+        firstname: 'Dani',
+        lastname: 'Perez'
+    };
+
+    res.end(JSON.stringify(obj));
 
 }).listen(3000, '192.168.178.227');
-
-// Better performance
-// fs.readFileSync(__dirname + '/index.html').pipe(res);
