dfa2387ba7a3c80acc2384c0ceb64bb59a060dfe
Dani <dani.93.perez@gmail.com>
2020-12-30 09:17:12 +0000

    middlewware to decode form or json values

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
+//http body parser
+var bodyParser = require('body-parser');
+
 var app = express();
 
 //How to deal with environment configurations
 var port = process.env.PORT || 3000;
 
+//Body parser
+var urlEncodedParser = bodyParser.urlencoded({ extended: false});
+var jsonParser = bodyParser.json();
 //Middleware
 app.use('/assets', express.static(__dirname + '/public'));
 
@@ -19,12 +25,28 @@ app.use('/', function(req, res, next) {
 });
 
 app.get('/', function(req, res) {
-    res.render('index', { ID: req.params.id });
+    res.render('index');
 });
 
 app.get('/person/:id', function(req, res) {
-    res.send(`<html><head></head><body><h1>Person: ${req.params.id}</h1></body></html>`);
+    // http://danielperezlorenzo.com:3000/person/dani?qstr=12
+    res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
 });
+
+//form url encoded
+app.post('/person', urlEncodedParser, function(req, res) {
+    res.send('Thank you!');
+    console.log(req.body.firstname); //.body added by the urlEncodedParser middleware
+    console.log(req.body.lastname);
+});
+
+//form url encoded
+app.post('/personJson', jsonParser, function(req, res) {
+    res.send('Thank you for the JSON data');
+    console.log(req.body.firstname); //.body added by the urlEncodedParser middleware
+    console.log(req.body.lastname);
+});
+
 
 app.get('/api', function(req, res) {
     res.json({ name: 'dani', server: 'node'});
