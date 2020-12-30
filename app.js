6cfffff600550035023c230f40c9aec6c5a3a492
Dani <dani.93.perez@gmail.com>
2020-12-30 08:57:36 +0000

    view engine

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ var port = process.env.PORT || 3000;
 //Middleware
 app.use('/assets', express.static(__dirname + '/public'));
 
+//Specify the view engine
+app.set('view engine', 'ejs');
+
 //Create your own middleware
+//If you don't specify any route it will happend to all
 app.use('/', function(req, res, next) {
     console.log('Request Url: '+ req.url);
     //Keep on going
@@ -15,11 +19,11 @@ app.use('/', function(req, res, next) {
 });
 
 app.get('/', function(req, res) {
-    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world</h1></body></html>')
+    res.render('index', { ID: req.params.id });
 });
 
 app.get('/person/:id', function(req, res) {
-    res.send(`<html><head></head><body><h1>Person: ${req.params.id}</h1></body></html>`)
+    res.send(`<html><head></head><body><h1>Person: ${req.params.id}</h1></body></html>`);
 });
 
 app.get('/api', function(req, res) {
