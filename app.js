81c3a07d467853a92ef8b5d2794017b7f19c8cc6
Dani <dani.93.perez@gmail.com>
2020-12-30 08:48:12 +0000

    middleware

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ var app = express();
 //How to deal with environment configurations
 var port = process.env.PORT || 3000;
 
+//Middleware
+app.use('/assets', express.static(__dirname + '/public'));
+
+//Create your own middleware
+app.use('/', function(req, res, next) {
+    console.log('Request Url: '+ req.url);
+    //Keep on going
+    next();
+});
+
 app.get('/', function(req, res) {
-    res.send('<html><head></head><body><h1>Hello world</h1></body></html>')
+    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world</h1></body></html>')
 });
 
 app.get('/person/:id', function(req, res) {
